0e410cc2f8d3e29f72956e54c1a131af0f5b4bb2
R-Ross414 <rross@chapman.edu>
2018-11-24 09:33:54 +0000

    commit 6

---
+++ Database.cpp
@@ -0,0 +1,25 @@
+#include "Database.h"
+#include <iostream>
+using namespace std;
+
+Database::Database()
+{
+  //students = new BST<Student*>();
+}
+
+Database::Database(BST<Student>* students)
+{
+  //this->students = students;
+}
+
+Database::~Database() {}
+
+void Database::printStudents()
+{
+  students->printTree();
+}
+
+void Database::addStudent(Student* student)
+{
+
+}
--- main.cpp
+++ main.cpp
@@ -1,11 +1,20 @@
 #include <iostream>
-#include "People.h"
+//#include "People.h"
+#include "Database.h"
+//#include "BST.h"
 #include <string>
 
 using namespace std;
 
 int main()
 {
-  Faculty* faculty = new Faculty(4, "name", "level", "dept", new BST<int>());
-  //Faculty* faculty = new Faculty();
+  //BST<int>* studentss = new BST<int>();
+  //studentss->insert(1);
+  //studentss->insert(2);
+  //studentss->insert(3);
+
+  //cout << studentss->search(3) << endl;
+  Database* data = new Database();
+  //Student* student = new Student(int ID, string name, string level, string major, double GPA, int advisor):Person(ID, name, level)
+  //Faculty* faculty = new Faculty(4, "name", "level", "dept", new BST<int>());
 }
